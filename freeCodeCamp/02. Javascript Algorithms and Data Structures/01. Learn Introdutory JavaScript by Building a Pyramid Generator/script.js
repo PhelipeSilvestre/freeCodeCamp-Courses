7d9d2f9c0b57473ab7ffab7fc13c8ed6cf31a7ce
Phelipe_Silvestre <phelipe17silvestre@gmail.com>
2024-06-19 20:36:02 -0300

    create a base of pyramid with a for loop

--- freeCodeCamp/02. Javascript Algorithms and Data Structures/01. Learn Introdutory JavaScript by Building a Pyramid Generator/script.js
+++ freeCodeCamp/02. Javascript Algorithms and Data Structures/01. Learn Introdutory JavaScript by Building a Pyramid Generator/script.js
@@ -1,1 +1,16 @@
-let character = 'Hello';
+const character = "#";
+const count = 8;
+const rows = []; // array to store the rows
+
+// loop to create the rows
+for (let i = 0; i < count; i = i + 1) {
+  rows.push(i);
+}
+
+let result = "";
+
+// loop to create the pyramid
+for (const row of rows) {
+}
+
+console.log(result);

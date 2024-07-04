50bbe4a8a0b2abb07665898e66a2e8a7940d60f9
phelipeSilvestre <99892687+PhelipeSilvestre@users.noreply.github.com>
2024-07-04 12:13:01 -0300

    add second activit - grade score

--- freeCodeCamp/02. Javascript Algorithms and Data Structures/02. Review Javascript Fundamentals by Buildind a Gradebook App/theGradebookApp.js
+++ freeCodeCamp/02. Javascript Algorithms and Data Structures/02. Review Javascript Fundamentals by Buildind a Gradebook App/theGradebookApp.js
@@ -1,3 +1,4 @@
+// Atividade I - Average of score
 function getAverage(scores) {
     let sum = 0;
     let average = 0;
@@ -14,3 +15,26 @@ function getAverage(scores) {
   
   console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
   console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+
+// atividade II - grade of score 
+function getGrade(score) {
+  
+  if(score === 100){
+    return "A++";
+  } else if (score >= 90 && score <= 99){
+    return "A";
+  } else if (score >= 80 && score <= 89){
+    return "B";
+  } else if (score >= 70 && score <= 79){
+    return "C";
+  } else if (score >= 60 && score <= 69){
+    return "D";
+  } else if (score >= 0 && score <= 59){
+    return "F";
+  }
+  return
+}
+
+console.log(getGrade(96));
+console.log(getGrade(82));
+console.log(getGrade(56));

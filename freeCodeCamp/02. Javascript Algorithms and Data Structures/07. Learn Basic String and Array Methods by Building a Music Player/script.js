6b303c2d222e329b31677f7511d21314d6550c17
phelipeSilvestre <99892687+PhelipeSilvestre@users.noreply.github.com>
2024-08-26 22:33:41 -0300

    Start create a function off play audio

--- freeCodeCamp/02. Javascript Algorithms and Data Structures/07. Learn Basic String and Array Methods by Building a Music Player/script.js
+++ freeCodeCamp/02. Javascript Algorithms and Data Structures/07. Learn Basic String and Array Methods by Building a Music Player/script.js
@@ -85,7 +85,19 @@ let userData = {
   songCurrentTime: 0,
 };
 
+const playSong = (id) => {
+  const song = userData?.songs.find((song) => song.id === id);
+  audio.src = song.src;
+  audio.title = song.title;
 
+  if (userData?.currentSong === null || userData?.currentSong.id !== song.id) {
+    audio.currentTime = 0;
+  }
+  else {
+    
+  }
+
+};
 
 const renderSongs = (array) => {
   const songsHTML = array

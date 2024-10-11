19b6107788d9a40b0f32ec9cf9bf68ee33246ef5
phelipeSilvestre <99892687+PhelipeSilvestre@users.noreply.github.com>
2024-10-10 21:18:08 -0300

    add function addPlayerCards

--- freeCodeCamp/02. Javascript Algorithms and Data Structures/09. Learn Modern JavaScript Methods by Building Football Team Cards/script.js
+++ freeCodeCamp/02. Javascript Algorithms and Data Structures/09. Learn Modern JavaScript Methods by Building Football Team Cards/script.js
@@ -179,3 +179,22 @@ typeOfSport.textContent = sport;
 teamName.textContent = team;
 worldCupYear.textContent = year;
 headCoach.textContent = coachName;
+
+const setPlayerCards = (arr = players) => {
+
+
+  playerCards.innerHTML += arr.map(
+    ({ name, position, number, isCaptain, nickname }) => {
+      `
+        <div class="player-card">
+          <h2>${isCaptain ? "(Captain)" : ""} ${name}</h2>
+          <p>Position: ${position}</p>
+          <p>Number: ${number}</p>
+          <p>Nickname: </p>
+        </div>
+      `;
+    }
+  );
+
+
+};

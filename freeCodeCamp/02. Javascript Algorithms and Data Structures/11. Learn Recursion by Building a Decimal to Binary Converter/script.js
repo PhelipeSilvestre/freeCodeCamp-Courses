18064805b5e1c29c895de7124b69bb80d27d7c04
phelipeSilvestre <99892687+PhelipeSilvestre@users.noreply.github.com>
2024-12-02 19:57:59 -0300

    finalizado a chamada recursiva

--- freeCodeCamp/02. Javascript Algorithms and Data Structures/11. Learn Recursion by Building a Decimal to Binary Converter/script.js
+++ freeCodeCamp/02. Javascript Algorithms and Data Structures/11. Learn Recursion by Building a Decimal to Binary Converter/script.js
@@ -2,50 +2,14 @@ const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
-const decimalToBinary = (input) => { 
 
-  // Modo mais eficiente de realizar a conversão decimal = binario
-  let binary = "";
-
-  if (input === 0) {
-    binary = "0";
+// Modo utilizando recursão
+const decimalToBinary = (input) => {
+  if (input === 0 || input === 1) {
+    return String(input);
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
-
-  while (input > 0) {
-    binary = (input % 2) + binary;
-    input = Math.floor(input / 2);
-  }
-
-  result.innerText = binary;
-
-  /* Essa foi a primeira função utilizada para converter decimal para binario
-
-  const inputs = [];
-  const quotients = [];
-  const remainders = [];
-
-  if (input === 0) {
-    result.innerText = "0";
-    return;
-  }
-
-  while (input > 0) {
-    const quotient = Math.floor(input / 2);
-    const remainder = input % 2;
-
-    inputs.push(input);
-    quotients.push(quotient);
-    remainders.push(remainder);
-    input = quotient;
-  }
-
-  console.log("Inputs: ", inputs);
-  console.log("Quotients: ", quotients);
-  console.log("Remainders: ", remainders);
-
-  result.innerText = remainders.reverse().join("");
-
-  */
 };
 
 const checkUserInput = () => {
@@ -58,7 +22,7 @@ const checkUserInput = () => {
     return;
   }
 
-  decimalToBinary(parseInt(numberInput.value));
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
   numberInput.value = "";
 };
 

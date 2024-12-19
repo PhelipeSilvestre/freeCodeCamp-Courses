098e7a846cb22abdbba50cccca82e9c07a41b136
phelipeSilvestre <99892687+PhelipeSilvestre@users.noreply.github.com>
2024-12-19 12:04:27 -0300

    add: setTimeout function

--- freeCodeCamp/02. Javascript Algorithms and Data Structures/11. Learn Recursion by Building a Decimal to Binary Converter/script.js
+++ freeCodeCamp/02. Javascript Algorithms and Data Structures/11. Learn Recursion by Building a Decimal to Binary Converter/script.js
@@ -3,7 +3,6 @@ const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 
-// Modo utilizando recursão
 const decimalToBinary = (input) => {
   if (input === 0 || input === 1) {
     return String(input);
@@ -12,17 +11,32 @@ const decimalToBinary = (input) => {
   }
 };
 
+const showAnimation = () => {
+  setTimeout(() => {
+    console.log("free");
+  }, 500);
+  setTimeout(() => {
+    console.log("Code");
+  }, 1000);
+  setTimeout(() => {
+    console.log("Camp");
+  }, 1500);
+};
+
 const checkUserInput = () => {
-  if (
-    !numberInput.value ||
-    isNaN(parseInt(numberInput.value)) ||
-    parseInt(numberInput.value) < 0
-  ) {
+  const inputInt = parseInt(numberInput.value);
+
+  if (!numberInput.value || isNaN(inputInt) || inputInt < 0) {
     alert("Please provide a decimal number greater than or equal to 0");
     return;
   }
 
-  result.textContent = decimalToBinary(parseInt(numberInput.value));
+  if (inputInt === 5) {
+    showAnimation();
+    return;
+  }
+
+  result.textContent = decimalToBinary(inputInt);
   numberInput.value = "";
 };
 

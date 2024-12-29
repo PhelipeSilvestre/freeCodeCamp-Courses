a308ba31d705174000884c4203591e91f48c9e3c
phelipeSilvestre <99892687+PhelipeSilvestre@users.noreply.github.com>
2024-12-29 11:28:51 -0300

    star in function w'll converter number

--- freeCodeCamp/02. Javascript Algorithms and Data Structures/Certification Project II - Build a Roman Numeral Converter /script.js
+++ freeCodeCamp/02. Javascript Algorithms and Data Structures/Certification Project II - Build a Roman Numeral Converter /script.js
@@ -1,3 +1,12 @@
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("output");
+
+
+const decimalToRomanNumeral = (input) => {
+    if (input <= 0) {
+      return "Numero invalido aqui";
+    } else {
+      return "Numero será convertido aqui";
+    }
+  };
